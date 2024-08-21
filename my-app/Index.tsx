38a9652edee94596e300cb99b55799a9079dbcda
CleevePhilip <147903474+CleevePhilip@users.noreply.github.com>
2024-08-21 23:41:20 +0800

    Modify

---
+++ my-app/Index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
